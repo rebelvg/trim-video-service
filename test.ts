887493b5ba4c84c38028c86a2b3272ea94c9b5b8
Rebel <johnhartigan@ya.ru>
2019-12-31 13:32:44 +0200

    Add test files.

---
+++ test.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import * as fs from 'fs';
+
+import { userService } from './common/services/user';
+
+(async () => {
+  const user = await userService.create();
+
+  const { data } = await axios.post(
+    'http://localhost:8000/v1/trim-tasks',
+    {
+      startTime: 0,
+      endTime: 1
+    },
+    {
+      headers: { token: user.token }
+    }
+  );
+
+  console.log(data);
+
+  await axios.put(`http://localhost:8000/v1/trim-tasks/${data.taskId}/upload-video`, fs.readFileSync('test.flv'), {
+    headers: { token: user.token }
+  });
+
+  console.log('upload done...');
+})();
